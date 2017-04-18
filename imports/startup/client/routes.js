9b2252ce1b903ba960bca275c3a44a35a354a74e
Lei JING <leijing7@gmail.com>
2017-04-18 20:09:29 +0800

    tabbar fixed

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,36 +6,35 @@ import {
 } from 'react-router-dom'
 
 const Home = () =>  
-  <div className="weui-tab">
-      <div className="weui-tab__panel">
-          <div>Page 1</div>
-          <div style={{display:"none"}}>Page 2</div>
-          <div style={{display:"none"}}>Page 3</div>
-      </div>
-      <div className="weui-tab">
-        <div className="weui-tab__panel">
-          <div>ddd</div>
-        </div>
-        <div className="weui-tabbar">
-            <a href="javascript:;" className="weui-tabbar__item weui-bar__item_on">
-                <i className="weui-icon-success weui-icon_msg"></i>
-                <p className="weui-tabbar__label">微信</p>
-            </a>
-            <a href="javascript:;" className="weui-tabbar__item">
-                <img src="./images/icon_tabbar.png" alt="" className="weui-tabbar__icon"/>
-                <p className="weui-tabbar__label">通讯录</p>
-            </a>
-            <a href="javascript:;" className="weui-tabbar__item">
-                <img src="./images/icon_tabbar.png" alt="" className="weui-tabbar__icon"/>
-                <p className="weui-tabbar__label">发现</p>
-            </a>
-            <a href="javascript:;" className="weui-tabbar__item">
-                <img src="./images/icon_tabbar.png" alt="" className="weui-tabbar__icon"/>
-                <p className="weui-tabbar__label">我</p>
-            </a>
-        </div>
+<div style={{height: '100%'}}>
+  <div className="weui-tab" style={{height: '100%'}}>
+    <div className="weui-tab__panel">
+      <div>ddd</div>
     </div>
-  </div>
+    <div className="weui-tabbar" style={{position: "fixed"}}>
+        <a href="javascript:;" className="weui-tabbar__item weui-bar__item_on">
+            <img src="/img/1.png" alt="" className="weui-tabbar__icon"/>
+            <p className="weui-tabbar__label">微信</p>
+        </a>
+        <a href="javascript:;" className="weui-tabbar__item">
+            <img src="/img/2.png" alt="" className="weui-tabbar__icon"/>
+            <p className="weui-tabbar__label">通讯录</p>
+        </a>
+        <a href="javascript:;" className="weui-tabbar__item">
+            <img src="/img/1.png" alt="" className="weui-tabbar__icon"/>
+            <p className="weui-tabbar__label">发现</p>
+        </a>
+        <a href="javascript:;" className="weui-tabbar__item">
+            <img src="/img/2.png" alt="" className="weui-tabbar__icon"/>
+            <p className="weui-tabbar__label">内容</p>
+        </a>
+        <a href="javascript:;" className="weui-tabbar__item">
+            <img src="/img/1.png" alt="" className="weui-tabbar__icon"/>
+            <p className="weui-tabbar__label">我</p>
+        </a>
+    </div>
+</div>
+</div>
 
 const About = () => <a href="javascript:;" className="weui-btn weui-btn_plain-default">按钮</a>
 
